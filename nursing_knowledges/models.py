32ed529c00d21eff5e713c214e3839dd30942abe
eunche <eunchae619@naver.com>
2021-10-21 13:18:23 +0900

    간호지식 모델 수정 -간호지식Model -> 질병,진단 Model로 분할 -질병or진단에서 manytomany 연결하면 다른쪽도 연결되도록 save method override -질병or진단 연결관계가 삭제되면 다른쪽도 삭제되도록 delete method override -질병<->진단 / 진단<->질병 모델이 내용이 똑같아서 Admin에는 하나만 보이도록 변경 -InterventionRelation 모델에서 ForeignKey를 질병/진단에서 connection으로 변경

--- nursing_knowledges/models.py
+++ nursing_knowledges/models.py
@@ -1,26 +1,95 @@
 from django.db import models
+from django.db.models import Q
 
-# Create your models here.
 
-class NursingKnowledge(models.Model):
+class Disease(models.Model):
     """
-    (간호 질병 + 간호 진단) Model
+    간호 질병 Model
     """
-    DISEASE = "DISEASE"  # 질병
-    DIAGNOSIS = "DIAGNOSIS"  # 진단
-    TYPES = [
-        (DISEASE, "Disease"),
-        (DIAGNOSIS, "Diagnosis"),
-    ]
-    
-    name = models.CharField(max_length=100)  # (질병,진단)명
-    type = models.CharField(max_length=20, choices=TYPES)
-    connection = models.ManyToManyField("self", blank=True)  # 연결 관계
+    name = models.CharField(max_length=100)  # 질병명
+    diagnoses = models.ManyToManyField("Diagnosis", blank=True, through="DiseaseConnect")  # 연결된 진단
 
     def __str__(self):
-        return f"[{self.type}]{self.name}"
+        return self.name
 
-class NursingInterventionContent(models.Model):
+class DiseaseConnect(models.Model):
+    """
+    질병 <-> 진단의 연결관계를 나타내는  through Model
+    """
+    disease = models.ForeignKey("Disease", on_delete=models.CASCADE)  # 연결된 질병
+    diagnosis = models.ForeignKey("Diagnosis", on_delete=models.CASCADE)  # 연결된 진단
+
+    def save(self, *args, **kwargs):
+        super(DiseaseConnect, self).save(*args, **kwargs)
+
+        # DiagnosisConnect 모델에 같은 연결관계가 있는지 확인
+        symmetry_object = DiagnosisConnect.objects.filter(Q(disease=self.disease) & Q(diagnosis=self.diagnosis))
+        if not symmetry_object.exists():
+            # 없으면 DiagnosisConnect 모델에 연결관계 생성
+            new_object = DiagnosisConnect.objects.create(disease=self.disease, diagnosis=self.diagnosis)
+            new_object.save()
+
+    def delete(self, *args, **kwargs):
+        disease = self.disease
+        diagnosis = self.diagnosis
+        super(DiseaseConnect, self).delete(*args, **kwargs)
+
+        # DiagnosisConnect 모델에 같은 연결관계가 있는지 확인
+        symmetry_object = DiagnosisConnect.objects.filter(Q(disease=disease) & Q(diagnosis=diagnosis))
+        if symmetry_object.exists():
+            # 있으면 DiagnosisConnect 모델의 관계도 삭제
+            symmetry_object.delete()
+
+    def __str__(self):
+        return f"[질병]{self.disease} <-> [진단]{self.diagnosis}"
+
+class Diagnosis(models.Model):
+    """
+    간호 진단 Model
+    """
+    name = models.CharField(max_length=100)  # 진단명
+    diseases = models.ManyToManyField("Disease", blank=True, through="DiagnosisConnect")  # 연결된 질병
+
+    class Meta:
+        verbose_name = "Diagnose"
+
+    def __str__(self):
+        return self.name
+
+class DiagnosisConnect(models.Model):
+    """
+    진단 <-> 질병의 연결관계를 나타내는 through Model
+    """
+    disease = models.ForeignKey("Disease", on_delete=models.CASCADE)  # 연결된 질병
+    diagnosis = models.ForeignKey("Diagnosis", on_delete=models.CASCADE)  # 연결된 진단
+
+    def save(self, *args, **kwargs):
+        super(DiagnosisConnect, self).save(*args, **kwargs)
+
+        # DiseaseConnect 모델에 같은 연결관계가 있는지 확인
+        symmetry_object = DiseaseConnect.objects.filter(Q(disease=self.disease) & Q(diagnosis=self.diagnosis))
+        if not symmetry_object.exists():
+            # 없으면 DiseaseConnect 모델에 연결관계 생성
+            new_object = DiseaseConnect.objects.create(disease=self.disease, diagnosis=self.diagnosis)
+            new_object.save()
+    
+    def delete(self, *args, **kwargs):
+        disease = self.disease
+        diagnosis = self.diagnosis
+        super(DiagnosisConnect, self).delete(*args, **kwargs)
+
+        # DiseaseConnect 모델에 같은 연결관계가 있는지 확인
+        symmetry_object = DiseaseConnect.objects.filter(Q(disease=disease) & Q(diagnosis=diagnosis))
+        if symmetry_object.exists():
+            # 있으면 DiseaseConnect 모델의 관계도 삭제
+            symmetry_object.delete()
+
+
+    
+    def __str__(self):
+        return f"{self.disease} <-> {self.diagnosis}"
+
+class InterventionContent(models.Model):
     """
     간호 중재 내용 Model
     """
@@ -30,14 +99,13 @@ class NursingInterventionContent(models.Model):
         return self.content[:50]
 
 
-class KnowledgeInterventionRelation(models.Model):
+class InterventionRelation(models.Model):
     """
-    어떤 질병의 -> 어떤 진단의 중재인지 관계를 나타내는 Model
+    어떤 질병/진단 관계의 중재인지를 나타내는 Model
     """
-    intervention = models.ForeignKey("NursingInterventionContent", on_delete=models.CASCADE, related_name="relation_content")  # 어떤 중재 내용의 관계를 나타낼지
-    disease = models.ForeignKey("NursingKnowledge", on_delete=models.CASCADE, related_name="relation_diseases")  # 어떤 질병의?
-    diagnosis = models.ForeignKey("NursingKnowledge", on_delete=models.CASCADE, related_name="relation_diagnosis")  # 어떤 진단의?
+    intervention = models.ForeignKey("InterventionContent", on_delete=models.CASCADE, related_name="intervention_relations")  # 어떤 중재 내용의 관계를 나타낼지
+    connection = models.ForeignKey("DiseaseConnect", on_delete=models.CASCADE, related_name="connection_relations")  # 어떤 질병/진단 관계의?
 
     def __str__(self):
-        return f"{self.disease} - {self.diagnosis} ==> {self.intervention}"
+        return f"({self.connection}) : {self.intervention}"
 
