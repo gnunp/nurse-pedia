b8f845eed9f909f42995170efa2b35be00456177
eunche <eunchae619@naver.com>
2021-11-16 15:09:29 +0900

    코드 리펙토링

--- static/users/js/userModal.js
+++ static/users/js/userModal.js
@@ -1,45 +1,61 @@
-const signInBtn = document.querySelector(".js-sign_in");  // Header - 로그인 버튼
-const signUpBtn = document.querySelector(".js-sign_up");  // Header - 회원가입 버튼
+/*
+전역 변수
+*/
+let signInModal; // 로그인 모달 Element
+let signUpModal; // 회원가입 모달 Element
 
-// 로그인 모달창을 띄우는 함수
-const handleClickSignInBtn = async (event) => {
+
+const showModal = (modalElement) => {
     // 모달 wrapper 생성
-    const modalWrapper = document.createElement('div');
-    modalWrapper.className = "user_modal_wrapper";
-    document.body.appendChild(modalWrapper);
+    const modalBackground = document.createElement('div');
+    modalBackground.className = "modal_background";
+    document.body.appendChild(modalBackground);
+
+    // 가져온 모달 HTML 현재 화면에 넣기
+    modalBackground.appendChild(modalElement);
+
+    // 모달 닫기 이벤트 리스너 추가
+    const modalCloseBtn = document.querySelector(".js-modal_close_btn");
+    modalCloseBtn.addEventListener("click", handleClickModalCloseBtn)
+}
+
+const handleClickModalCloseBtn = (event) => {
+    document.querySelector(".modal_background").remove();
+}
+
+const handleClickSignInBtn = (event) => {
+    showModal(signInModal);
+}
+
+const handleClickSignUpBtn = (event) => {
+    alert("회원가입")
+}
+
+
+
+const userModalInit = async () => {
+    const signInBtn = document.querySelector(".js-sign_in");  // Header - 로그인 버튼
+    const signUpBtn = document.querySelector(".js-sign_up");  // Header - 회원가입 버튼
     
-    // 로그인 모달 HTML text로 가져오기
-    const response = await fetch('/users/signin-modal');
-    const signInModalHtml = await response.text();
+    // 이벤트 리스너 등록
+    signInBtn.addEventListener("click", handleClickSignInBtn);
+    signUpBtn.addEventListener("click", handleClickSignUpBtn);
+
+
+    // 모달 HTML text로 가져오기
+    const SignInModalResponse = await fetch('/users/signin-modal');
+    const signInModalHtml = await SignInModalResponse.text();
 
     // DOM Parser 생성
     const parser = new DOMParser();
 
-    // 로그인 모달 DOM으로 변환
-    const signInModalDocument = parser.parseFromString(signInModalHtml, "text/html");
+    // 모달 DOM으로 변환
+    signInDocument = parser.parseFromString(signInModalHtml, "text/html");
 
-    // 가져온 로그인 모달 HTML 현재 화면에 넣기
-    const signInModalWrapper = signInModalDocument.querySelector(".modal__signin_wrapper")
-    modalWrapper.appendChild(signInModalWrapper);
+    // 모달 Element
+    signInModal = signInDocument.querySelector(".modal_wrapper");
 
-    // 모달 닫기 이벤트 리스너 추가
-    const signInModalCloseBtn = document.querySelector(".js-signin_close_btn");
-    signInModalCloseBtn.addEventListener("click", handleClickModalCloseBtn)
 }
 
-// 회원가입 모달창을 띄우는 함수
-const handleClicksignUpBtn = (event) => {
-    alert("회원가입")
-}
+userModalInit();
 
-// 모달창을 닫는 함수
-const handleClickModalCloseBtn = (event) => {
-    document.querySelector(".user_modal_wrapper").remove();
-}
-
-
-/*
-이벤트 리스너
-*/
-signInBtn.addEventListener("click", handleClickSignInBtn);
-signUpBtn.addEventListener("click", handleClicksignUpBtn);
