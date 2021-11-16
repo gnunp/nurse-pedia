bf5b22c786245855d8a40e0e8183f1c356335fa6
eunche <eunchae619@naver.com>
2021-11-16 15:28:07 +0900

    로그인/회원가입 모달 창 띄우는 기능 구현 -실제 회원가입/로그인/소셜로그인 구현은 구현하지 x

--- static/users/js/userModal.js
+++ static/users/js/userModal.js
@@ -4,6 +4,31 @@
 let signInModal; // 로그인 모달 Element
 let signUpModal; // 회원가입 모달 Element
 
+const deleteModal = () => {
+    document.querySelector(".modal_background").remove();
+}
+
+const handleClickModalCloseBtn = (event) => {
+    deleteModal();
+}
+
+const modalToggle = () => {
+    const currentModal = document.querySelector(".modal_wrapper");
+    // if 현재 띄워져있는 모달이 로그인 모달일 경우
+    if(Object.is(currentModal, signInModal)){
+        deleteModal();
+        showModal(signUpModal);
+    }
+    // else 회원가입 모달일 경우
+    else{
+        deleteModal();
+        showModal(signInModal);
+    }
+}
+
+const handleClickModalToggleBtn = (event) => {
+    modalToggle();
+}
 
 const showModal = (modalElement) => {
     // 모달 wrapper 생성
@@ -17,10 +42,10 @@ const showModal = (modalElement) => {
     // 모달 닫기 이벤트 리스너 추가
     const modalCloseBtn = document.querySelector(".js-modal_close_btn");
     modalCloseBtn.addEventListener("click", handleClickModalCloseBtn)
-}
 
-const handleClickModalCloseBtn = (event) => {
-    document.querySelector(".modal_background").remove();
+    // 모달 토글 이벤트 리스너 추가
+    const modalToggleBtn = document.querySelector(".js-toggle_btn");
+    modalToggleBtn.addEventListener("click", handleClickModalToggleBtn);
 }
 
 const handleClickSignInBtn = (event) => {
@@ -28,7 +53,7 @@ const handleClickSignInBtn = (event) => {
 }
 
 const handleClickSignUpBtn = (event) => {
-    alert("회원가입")
+    showModal(signUpModal);
 }
 
 
@@ -43,17 +68,23 @@ const userModalInit = async () => {
 
 
     // 모달 HTML text로 가져오기
-    const SignInModalResponse = await fetch('/users/signin-modal');
-    const signInModalHtml = await SignInModalResponse.text();
+    const signInModalResponse = await fetch('/users/signin-modal');
+    const signInModalHtml = await signInModalResponse.text();
+
+    const signUpModalResponse = await fetch('/users/signup-modal');
+    const signUpModalHtml = await signUpModalResponse.text();
+
 
     // DOM Parser 생성
     const parser = new DOMParser();
 
     // 모달 DOM으로 변환
     signInDocument = parser.parseFromString(signInModalHtml, "text/html");
+    signUpDocument = parser.parseFromString(signUpModalHtml, "text/html");
 
     // 모달 Element
     signInModal = signInDocument.querySelector(".modal_wrapper");
+    signUpModal = signUpDocument.querySelector(".modal_wrapper");
 
 }
 
