220bffef493df857d9dbf475dd02cf99e35aea19
eunche <eunchae619@naver.com>
2021-11-21 01:29:29 +0900

    질병(소분류)/진단 디테일 페이지 추가 -정적 페이지 -Django Template Variable로 관련 데이터 연결한 상태

--- nursing_knowledges/urls.py
+++ nursing_knowledges/urls.py
@@ -4,4 +4,6 @@ from .views import views
 app_name = 'nursing_knowledges'
 
 urlpatterns = [
+    path('disease/<int:pk>', views.disease_detail, name='disease_detail'),
+    path('diagnosis/<int:pk>', views.diagnosis_detail, name='diagnosis_detail'),
 ]
