9922e0d7f0474ffd2b1323fdd3e05d715020b63b
eunche <eunchae619@naver.com>
2022-03-21 00:19:12 +0900

    Feat: [Add]비로그인시 편집 버튼 누르면 로그인 Toast Message 뜨게 기능 추가

--- static/nursing_knowledges/js/diagnosis_detail.js
+++ static/nursing_knowledges/js/diagnosis_detail.js
@@ -1,16 +1,30 @@
 import Secondpage from "./secondpage.js";
 import {headerHeight} from '../../global/js/global';
+import {toastMessage} from "./toastMessage";
 
 class DiagnosisDetail{
     constructor(){
         this.setInit();
+
+        // 비로그인 편집 방지하는 코드
+        if(document.querySelector('.js-block_to_edit')){
+            for (const editBtn of document.querySelectorAll('.js-block_to_edit')) {
+                editBtn.addEventListener("click",this.handleClickEditBtn__toBlockEdit);
+            }
+        }
+
         this.findNode =document.querySelector('.js-knowledge_name').textContent;
-        new Secondpage(true, this.findNode);
+        // new Secondpage(true, this.findNode);
     }
 
     setInit(){
         this.diagnosisDetailWrap = document.querySelector('.root');
         this.diagnosisDetailWrap.style.top =`${headerHeight}px`;
+    }
+
+    handleClickEditBtn__toBlockEdit(event){
+        event.preventDefault();
+        toastMessage("로그인이 필요합니다");
     }
 }
 
--- static/nursing_knowledges/js/disease_detail.js
+++ static/nursing_knowledges/js/disease_detail.js
@@ -1,10 +1,13 @@
 import Secondpage from "./secondpage.js";
 import {headerHeight} from '../../global/js/global';
 import { shuffle } from "lodash";
+import {toastMessage} from "./toastMessage";
 
 class DiseaseDetail{
     constructor(){
         this.setInit();
+
+        // 관련 진단 아이템들 색 랜덤으로 부여하는 코드
         this.colors = [
             '#D8E2DC',
             '#FFE5D9',
@@ -12,13 +15,18 @@ class DiseaseDetail{
             '#F4ACB7',
             '#C6B5BA',
         ]
-
-
         this.relativeDiagnoses = document.querySelectorAll('.relative_diagnoses__item');
         if(this.relativeDiagnoses){
             this.colorSet();
         }
 
+        // 비로그인 편집 방지하는 코드
+        if(document.querySelector('.js-block_to_edit')){
+            document.querySelector('.js-block_to_edit')
+                .addEventListener("click",this.handleClickEditBtn__toBlockEdit);
+        }
+
+        // 마인드맵 그리는 코드
         this.findNode =document.querySelector('.js-knowledge_name').textContent;
         new Secondpage(true, this.findNode);
     }
@@ -34,6 +42,10 @@ class DiseaseDetail{
             }
             diagnosis.style.backgroundColor=this.colors[index % this.colors.length];
         });
+    }
+    handleClickEditBtn__toBlockEdit(event){
+        event.preventDefault();
+        toastMessage("로그인이 필요합니다");
     }
 }
 
