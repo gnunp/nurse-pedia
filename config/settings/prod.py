2cc25fa2adcee609ae6aa4bd31087c3324131577
eunche <eunchae619@naver.com>
2022-03-25 15:07:03 +0900

    Chore: prod.py 배포 IP 변경 및 DB설정 제거(임시)

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,19 +1,19 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.38.119.71', 'localhost', '.nurpi.co.kr']
+ALLOWED_HOSTS = ['34.204.36.26', 'localhost', '.nurpi.co.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
 
-MY_URL = "https://nurpi.co.kr"
+MY_URL = "34.204.36.26"
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nurpi-db',
-        'USER': 'postgres',
-        'PASSWORD': 'QZepqvTLad',
-        'HOST': 'nurpi-db.ceosuratl2js.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'nurpi-db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'QZepqvTLad',
+#         'HOST': 'nurpi-db.ceosuratl2js.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
