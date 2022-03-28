4d93f8709b1d05f0b8243814509cd91cca6ed4bc
Sun Ho <tjsgh5312@daum.net>
2022-03-28 23:11:01 +0900

    mypage 내가 찜한페이지 & 내가 열어본 페이지(style미적용)

--- static/users/js/mypage.js
+++ static/users/js/mypage.js
@@ -9,16 +9,22 @@ class Mypage{
 
         this.currentActiveNavitem = document.querySelector('.active');
     }
+    
+    setInit(){
+        const mypage = document.querySelector('.mypage_wrap');
+        mypage.style.top = `${headerHeight}px`;
 
+        /*----------html 미리 만드는 코드----------- */
+        this.createstarcontent();
+        this.createhistorycontent();
+    }
+
+    /*-------------------------내가 찜한 페이지 document element만드는 코드 ----------------- */
     async createstarcontent(){
         const starknowledge = await fetch('/api/knowledges/user-star-knowledges/');
-        console.log(starknowledge);
         const jsonstarknowledge = await starknowledge.json();
-    
-        const starcontent = document.createElement('div');
-        starcontent.classList.add('starcontent');
+        const starcontent = document.querySelector('.star_info');
         
-
         let url, type, id, name;
         jsonstarknowledge.forEach(element => {
             console.log(JSON.stringify(element));
@@ -41,10 +47,8 @@ class Mypage{
             starcontent.appendChild(this.createstaritem(name, url)); 
         });
 
-        console.log(starcontent);
-        document.querySelector('.content').appendChild(starcontent);
+        starcontent.classList.add('disappear');
     }
-
     createstaritem(name, url){
         const newitem = document.createElement('div');
         newitem.classList.add('staritem');
@@ -55,9 +59,37 @@ class Mypage{
         return newitem;
     }
 
+    /*-----------------내가 열어본 페이지 document element 만드는 코드 ----------------- */
+    createhistorycontent(){
+        const historycontent = document.querySelector('.history_info');
+
+        if(localStorage.length>0){
+            const pagehistory = JSON.parse(localStorage.getItem('pageHistory'));
+            pagehistory.forEach(element => {
+                console.log(element);
+                historycontent.appendChild(this.createhistoryitem(element));
+            });
+        }
+
+        historycontent.classList.add('disappear');
+    }
+    createhistoryitem(ele){
+        const name = ele.name;
+        const address = ele.address;
+        const newitem = document.createElement('div');
+        newitem.classList.add('historyitem');
+
+        newitem.innerHTML=`
+            <a href=${address}><h1>${name}</h1></a>
+        `
+        return newitem;
+    }
+
+    /*------------------------------왼쪽 global_nav바 클릭 이벤트-----------------------*/
     navClickevent(){
         this.navitems = document.querySelectorAll('.nav_item');
         const content_title = document.querySelector('.mypage_content h1');
+        let currentinfo = document.querySelector('.update_info');
 
         this.navitems.forEach(element => {
             element.addEventListener('click', (e)=>{
@@ -73,21 +105,39 @@ class Mypage{
                 content_title.textContent = `${activetitle}`;
 
                 /*-------------main content수정 ------------*/
-                if(activetitle == "내가 찜한 페이지"){
-                    this.createstarcontent();
+                switch (activetitle) {
+                    case "내 정보 수정":
+                        currentinfo.classList.add('disappear');
+                        currentinfo = document.querySelector('.update_info');
+                        currentinfo.classList.remove('disappear');
+                        break;
+
+                    case "내가 찜한 페이지":
+                        currentinfo.classList.add('disappear');
+                        currentinfo = document.querySelector('.star_info');
+                        currentinfo.classList.remove('disappear');
+                        break;
+
+                    case "내가 열어본 페이지":
+                        currentinfo.classList.add('disappear');
+                        currentinfo = document.querySelector('.history_info');
+                        currentinfo.classList.remove('disappear');
+                        break;
+                    case "개발자에게 쪽지 보내기":
+                        currentinfo.classList.add('disappear');
+                        currentinfo = document.querySelector('.develop_info');
+                        currentinfo.classList.remove('disappear');
+                        break;
+                    default:
+                        break;
                 }
-            
+                console.log(currentinfo);
             })
         });
     }
 
     setStarKnowledges(){
 
-    }
-
-    setInit(){
-        const mypage = document.querySelector('.mypage_wrap');
-        mypage.style.top = `${headerHeight}px`;
     }
 }
 
