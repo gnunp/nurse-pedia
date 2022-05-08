48f8cad1d9c30965d6b3c89ea12228c1cd88a9d1
eunche <eunchae619@naver.com>
2022-05-09 01:20:20 +0900

    Fix: 카카오 로그인 안되던 버그 수정

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('signin', views.signin, name="signin"),
     path("signin/kakao", views.kakao_signin, name="kakao_signin"),
     path("signin/kakao/callback/", views.kakao_callback, name="kakao_callback"),
-    path("signin/kakao/add-more-info", views.kakao_add_more_info, name="kakao_add_more_info"),
     path('signup', views.signup, name="signup"),
     path('logout', views.log_out, name="logout"),
     path('mypage', views.mypage, name="mypage"),
